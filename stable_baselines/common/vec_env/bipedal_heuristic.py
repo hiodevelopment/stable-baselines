7e8d5a9fb71de91d57c057cfe73d5513fc252ac3
Kence Anderson <kence.anderson@wisr.edu>
2020-01-01 13:24:22 -0800

    Updated bipdedal_hueristic agent with continuous curiosity mask

--- stable_baselines/common/vec_env/bipedal_heuristic.py
+++ stable_baselines/common/vec_env/bipedal_heuristic.py
@@ -280,7 +280,8 @@ class BipedalWalker(gym.Env, EzPickle):
 
         high = np.array([np.inf] * 24)
 
-        self.action_space = spaces.Box(np.array([-1, -1, -1, -1]), np.array([1, 1, 1, 1]), dtype=np.float32)
+        #self.action_space = spaces.Box(np.array([-1, -1, -1, -1]), np.array([1, 1, 1, 1]), dtype=np.float32)
+        self.action_space = spaces.Box(np.array([-0.1, -0.1, -0.1, -0.1]), np.array([0.1, 0.1, 0.1, 0.1]), dtype=np.float32)
 
         self.observation_space = spaces.Box(-high, high, dtype=np.float32)
 
@@ -748,9 +749,159 @@ class BipedalWalker(gym.Env, EzPickle):
 
 
 
-    def step(self, action):
+    def step(self, a):
 
         #self.hull.ApplyForceToCenter((0, 20), True) -- Uncomment this to receive a bit of stability help
+
+        ###### Get expert system actions, then apply epsilon modifications to them #########
+
+        pos = self.hull.position
+
+        vel = self.hull.linearVelocity
+
+        current_state = [
+
+            self.hull.angle,        # Normal angles up to 0.5 here, but sure more is possible.
+
+            2.0*self.hull.angularVelocity/FPS,
+
+            0.3*vel.x*(VIEWPORT_W/SCALE)/FPS,  # Normalized to get -1..1 range
+
+            0.3*vel.y*(VIEWPORT_H/SCALE)/FPS,
+
+            self.joints[0].angle,   # This will give 1.1 on high up, but it's still OK (and there should be spikes on hiting the ground, that's normal too)
+
+            self.joints[0].speed / SPEED_HIP,
+
+            self.joints[1].angle + 1.0,
+
+            self.joints[1].speed / SPEED_KNEE,
+
+            1.0 if self.legs[1].ground_contact else 0.0,
+
+            self.joints[2].angle,
+
+            self.joints[2].speed / SPEED_HIP,
+
+            self.joints[3].angle + 1.0,
+
+            self.joints[3].speed / SPEED_KNEE,
+
+            1.0 if self.legs[3].ground_contact else 0.0
+
+            ]
+
+        STAY_ON_ONE_LEG, PUT_OTHER_DOWN, PUSH_OFF = 1,2,3
+
+        SPEED = 0.29  # Will fall forward on higher speed [ this will eventually be a config.]
+
+        gait = STAY_ON_ONE_LEG
+
+        moving_leg = 0
+
+        supporting_leg = 1 - moving_leg
+
+        SUPPORT_KNEE_ANGLE = +0.1
+
+        supporting_knee_angle = SUPPORT_KNEE_ANGLE
+
+        s = current_state
+
+        #contact0 = s[8]
+
+        #contact1 = s[13]
+
+        moving_s_base = 4 + 5*moving_leg
+
+        supporting_s_base = 4 + 5*supporting_leg
+
+        hip_targ  = [None,None]   # -0.8 .. +1.1
+
+        knee_targ = [None,None]   # -0.6 .. +0.9
+
+        hip_todo  = [0.0, 0.0]
+
+        knee_todo = [0.0, 0.0]
+
+        if gait==STAY_ON_ONE_LEG:
+
+            hip_targ[moving_leg]  = 1.1
+
+            knee_targ[moving_leg] = -0.6
+
+            supporting_knee_angle += 0.03
+
+            if s[2] > SPEED: supporting_knee_angle += 0.03
+
+            supporting_knee_angle = min( supporting_knee_angle, SUPPORT_KNEE_ANGLE )
+
+            knee_targ[supporting_leg] = supporting_knee_angle
+
+            if s[supporting_s_base+0] < 0.10: # supporting leg is behind
+
+                state = PUT_OTHER_DOWN
+
+        if gait==PUT_OTHER_DOWN:
+
+            hip_targ[moving_leg]  = +0.1
+
+            knee_targ[moving_leg] = SUPPORT_KNEE_ANGLE
+
+            knee_targ[supporting_leg] = supporting_knee_angle
+
+            if s[moving_s_base+4]:
+
+                state = PUSH_OFF
+
+                supporting_knee_angle = min( s[moving_s_base+2], SUPPORT_KNEE_ANGLE )
+
+        if gait==PUSH_OFF:
+
+            knee_targ[moving_leg] = supporting_knee_angle
+
+            knee_targ[supporting_leg] = +1.0
+
+            if s[supporting_s_base+2] > 0.88 or s[2] > 1.2*SPEED:
+
+                state = STAY_ON_ONE_LEG
+
+                moving_leg = 1 - moving_leg
+
+                supporting_leg = 1 - moving_leg
+
+        
+        if hip_targ[0]: hip_todo[0] = 0.9*(hip_targ[0] - s[4]) - 0.25*s[5]
+
+        if hip_targ[1]: hip_todo[1] = 0.9*(hip_targ[1] - s[9]) - 0.25*s[10]
+
+        if knee_targ[0]: knee_todo[0] = 4.0*(knee_targ[0] - s[6])  - 0.25*s[7]
+
+        if knee_targ[1]: knee_todo[1] = 4.0*(knee_targ[1] - s[11]) - 0.25*s[12]
+
+
+        hip_todo[0] -= 0.9*(0-s[0]) - 1.5*s[1] # PID to keep head strait
+
+        hip_todo[1] -= 0.9*(0-s[0]) - 1.5*s[1]
+
+        knee_todo[0] -= 15.0*s[3]  # vertical speed, to damp oscillations
+
+        knee_todo[1] -= 15.0*s[3]
+
+        #a = np.array([action[0], action[1], action[2], action[3]])
+
+        a[0] += hip_todo[0]
+
+        a[1] += knee_todo[0]
+
+        a[2] += hip_todo[1]
+
+        a[3] += knee_todo[1]
+
+        a = np.clip(0.5*a, -1.0, 1.0)
+
+        action = a
+
+        ###################################################################################
 
         control_speed = False  # Should be easier as well
 
