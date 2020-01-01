eb51e406c26f5177050069e915fab9f31f7b674c
Kence Anderson <kence.anderson@wisr.edu>
2020-01-01 13:49:09 -0800

    Continuous Curiosity Mask

---
+++ curiosity_mask/run_continuous.py
@@ -0,0 +1,23 @@
+import gym
+import numpy as np
+import stable_baselines.common.vec_env.bipedal_heuristic as bipedal
+
+from stable_baselines.sac.policies import MlpPolicy
+from stable_baselines import SAC
+from stable_baselines.common.vec_env import DummyVecEnv
+
+env = DummyVecEnv([bipedal.BipedalWalker])
+
+model = SAC(MlpPolicy, env, verbose=1, tensorboard_log="run/")
+model.learn(total_timesteps=50000, log_interval=10)
+model.save("continuous_curiosity_mask")
+
+#del model # remove to demonstrate saving and loading
+
+#model = SAC.load("sac_pendulum")
+
+obs = env.reset()
+while True:
+    action, _states = model.predict(obs)
+    obs, rewards, dones, info = env.step(action)
+    env.render()
