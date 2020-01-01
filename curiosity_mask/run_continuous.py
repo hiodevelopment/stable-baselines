eef7c98d3360d017dc1ce78fe33f3d4533f84b12
Kence Anderson <kence.anderson@wisr.edu>
2020-01-01 15:09:48 -0800

    Update run_continuous.py

--- curiosity_mask/run_continuous.py
+++ curiosity_mask/run_continuous.py
@@ -9,15 +9,17 @@ from stable_baselines.common.vec_env import DummyVecEnv
 env = DummyVecEnv([bipedal.BipedalWalker])
 
 model = SAC(MlpPolicy, env, verbose=1, tensorboard_log="run/")
-model.learn(total_timesteps=50000)
+model.learn(total_timesteps=50)
 #model.save("continuous_curiosity_mask")
 
 #del model # remove to demonstrate saving and loading
 
 #model = SAC.load("sac_pendulum")
 
-obs = env.reset()
-while True:
-    action, _states = model.predict(obs)
-    obs, rewards, dones, info = env.step(action)
-    env.render()
+# Enjoy trained agent
+for _ in range(25):
+    obs, dones = env.reset(), [False], []
+    for i in range(1000):
+        action, _states = model.predict(obs)
+        obs, rewards, dones, info = env.step(action)
+        env.render()
