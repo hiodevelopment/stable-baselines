07c181b0920072d3b53e17600a1c81b6fa372417
Kence Anderson <kence.anderson@wisr.edu>
2020-01-01 21:03:06 -0800

    Update run_continuous.py

--- curiosity_mask/run_continuous.py
+++ curiosity_mask/run_continuous.py
@@ -1,26 +1,28 @@
-import gym
-import numpy as np
-import stable_baselines.common.vec_env.bipedal_heuristic as bipedal
+#import stable_baselines.common.vec_env.bipedal_heuristic as bipedal
 
-from stable_baselines import SAC
-from stable_baselines.sac.policies import MlpPolicy
 #from stable_baselines.common.vec_env import DummyVecEnv
 
 #env = DummyVecEnv([bipedal.BipedalWalker])
-env = gym.make('Pendulum-v0')
 
-model = SAC(MlpPolicy, env, verbose=1, tensorboard_log="run/")
-model.learn(total_timesteps=500)
-#model.save("continuous_curiosity_mask")
+import gym
 
-#del model # remove to demonstrate saving and loading
+from stable_baselines.common.policies import MlpPolicy
+from stable_baselines.common import make_vec_env
+from stable_baselines import A2C
 
-#model = SAC.load("sac_pendulum")
+# Parallel environments
+env = make_vec_env('Pendulum-v0', n_envs=4)
 
-# Enjoy trained agent
-for _ in range(25):
-    obs = env.reset()
-    for i in range(1000):
-        action, _states = model.predict(obs)
-        obs, rewards, dones, info = env.step(action)
-        env.render()
+model = A2C(MlpPolicy, env, verbose=1)
+model.learn(total_timesteps=25000)
+model.save("a2c_cartpole")
+
+del model # remove to demonstrate saving and loading
+
+model = A2C.load("a2c_cartpole")
+
+obs = env.reset()
+while True:
+    action, _states = model.predict(obs)
+    obs, rewards, dones, info = env.step(action)
+    env.render()
